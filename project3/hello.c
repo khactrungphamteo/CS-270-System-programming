5c0aee6d39e3eb459d1091704dfe87f375abbda6
khactrungphamteo <56216053+khactrungphamteo@users.noreply.github.com>
2021-05-11 20:03:06 -0400

    Add files via upload

---
+++ project3/hello.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    char string[100];
+    fgets(string, 100, stdin);
+    printf("hello world\n");
+    printf("argument: %s\n", argv[1]);
+    printf("argument: %s\n", argv[2]);
+    printf("%s\n", string);
+    return 0;
+}
